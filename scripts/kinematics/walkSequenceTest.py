f1367518341c7d3924de637e976c15c87e5148c9
Nickel-nc <nickel-nc@inbox.ru>
2023-04-11 00:08:08 +0300

    refactor for class wrappers. Add map reference

--- scripts/kinematics/walkSequenceTest.py
+++ scripts/kinematics/walkSequenceTest.py
@@ -1,106 +1,78 @@
+
 import os, sys
 sys.path.append(os.getcwd())
 
-from scripts.kinematics.hexapod.solvers.walkSequenceSolver import getWalkSequence
-from config.ik_settings import BASE_DIMENSIONS, BASE_POS, SERVO_ORDERED_LEG_NAMES
-
-from scripts.boardControl import BusCmd as cmd
-import time
-
 import numpy as np
 
-def translateToServos(sequence):
-    # TODO MOVE EXTERNAL FUNC
+from scripts.boardControl.BusCmd import BusCmd
+from scripts.kinematics.hexapod.solvers.walkSequenceSolver import getWalkSequence
+from config.ik_settings import (BASE_POS, SERVO_ORDERED_LEG_NAMES,
+                                LEGS_INCREMENT_CORRECTION_MATRIX, BASE_WALK_PARAMS)
 
-    sequence_len = len(sequence['leftFront']['alpha'])
-    servo_commands_sequence = np.full((sequence_len, 18), 500)
-    for leg_idx, leg_name in enumerate(SERVO_ORDERED_LEG_NAMES):
-        for seq_idx in range(sequence_len):
-            servo_commands_sequence[seq_idx][leg_idx*3 + 0] = round(sequence[leg_name]['alpha'][seq_idx],1)
-            servo_commands_sequence[seq_idx][leg_idx*3 + 1] = round(sequence[leg_name]['beta'][seq_idx],1)
-            servo_commands_sequence[seq_idx][leg_idx*3 + 2] = round(sequence[leg_name]['gamma'][seq_idx],1)
-    # print(servo_commands_sequence)
-    servo_commands_sequence = anglesToPositionsIncrements(servo_commands_sequence)
+class LegTranslator():
+    """
+    Translate leg angles (alpha, beta, gamma)
+    to servo Pulse for each leg (servos 1-18)
 
-    servo_commands_sequence *= INCREMENT_CORRECTION_DIMENTIONS
-    servo_commands_sequence += BASE_POS
-    # print("servo_commands_sequence", servo_commands_sequence)
+    For current state it separated from end-effector translator
+    """
 
-    return servo_commands_sequence
+    def __init__(self, cmd):
 
+        self.cmd = cmd
+        self.current_pos = BASE_POS  # zero state pos
 
+    def translateToServos(self, sequence):
 
-# solver -> controller
-TRANSLATE_POS_ORDER = [4,3,2,5,0,1]
+        sequence_len = len(sequence['leftFront']['alpha'])
+        servo_commands_sequence = np.full((sequence_len, 18), 500)
+        for leg_idx, leg_name in enumerate(SERVO_ORDERED_LEG_NAMES):
+            for seq_idx in range(sequence_len):
+                servo_commands_sequence[seq_idx][leg_idx*3 + 0] = round(sequence[leg_name]['alpha'][seq_idx],1)
+                servo_commands_sequence[seq_idx][leg_idx*3 + 1] = round(sequence[leg_name]['beta'][seq_idx],1)
+                servo_commands_sequence[seq_idx][leg_idx*3 + 2] = round(sequence[leg_name]['gamma'][seq_idx],1)
+        # print(servo_commands_sequence)
+        servo_commands_sequence = self.anglesToPositionsIncrements(servo_commands_sequence)
 
+        servo_commands_sequence *= LEGS_INCREMENT_CORRECTION_MATRIX
+        servo_commands_sequence += BASE_POS
+        # print("servo_commands_sequence", servo_commands_sequence)
 
-def anglesToPositionsIncrements(angles_bias: np.array) -> np.array:
-    """Convert angle increment to position increment"""
-    return (1000 / 240 * angles_bias).round().astype('int')
+        return servo_commands_sequence
 
+    # solver -> controller
 
-def convert_poses_order(poses):
-    converted = np.zeros(18)
-    for i, leg_num in enumerate(TRANSLATE_POS_ORDER):
-        converted[i * 3] = poses[leg_num]['coxia']
-        converted[i * 3 + 1] = poses[leg_num]['femur']
-        converted[i * 3 + 2] = poses[leg_num]['tibia']
+    def anglesToPositionsIncrements(self, angles_bias: np.array) -> np.array:
+        """Convert angle increment to position increment"""
+        return (1000 / 240 * angles_bias).round().astype('int')
 
-    return converted
+    def run_pos(self, command, rate):
 
-INCREMENT_CORRECTION_DIMENTIONS = np.array([
-    1, -1, 1,
-    1, -1, 1,
-    1, -1, 1,
-    1, 1, -1,
-    1, 1, -1,
-    1, 1, -1
-])
+        if np.any(command > 1000) or np.any(command < 0):
+            print("Error Positions", command)
+            return -1
 
-
-
-def transform_rotation_base(positions):
-    # transform matrix
-    return positions * INCREMENT_CORRECTION_DIMENTIONS
-
-
-def translate_poses(angle_increments):
-    """Translate angle increment to final servo positions"""
-    raw_pos_increments = anglesToPositionsIncrements(angle_increments)
-    correct_pos_increments = transform_rotation_base(raw_pos_increments)
-    new_positions = BASE_POS + correct_pos_increments
-    return new_positions
-
-
-def update_pos(poses, rate):
-    angle_increments = convert_poses_order(poses)
-    new_pos = translate_poses(angle_increments)
-
-    if np.any(new_pos > 1000) or np.any(new_pos < 0):
-        print("Error Positions", new_pos)
+        self.cmd.moveServos([i + 1 for i in range(18)], command, rate)
         return 0
 
-    cmd.moveServos([i + 1 for i in range(18)], new_pos, rate)
-    return new_pos
 
-def run_pos(command, rate):
-
-    if np.any(command > 1000) or np.any(command < 0):
-        print("Error Positions", command)
-        return -1
-
-    cmd.moveServos([i + 1 for i in range(18)], command, rate)
-    return 0
+    def run_sequence(self, sequence, n_times, rate, rev=1):
+        servo_commands_sequence = self.translateToServos(sequence)
+        for i in range(n_times):
+            for pos in servo_commands_sequence[::rev]:
+                self.run_pos(pos, rate)
 
 
-def get_initial_stance():
-    cmd.moveServos([i + 1 for i in range(18)], BASE_POS, 500)
-    time.sleep(0.5)
+class LegMovement():
+    """
+    Wrapper to control leg moves through simple encapsulated methods
+    """
 
-cases = [
-    {
-        "params": {
-            "dimensions": BASE_DIMENSIONS,
+    def __init__(self, cmd, params = BASE_WALK_PARAMS):
+        """
+        available parameters:
+
+            "dimensions": BASE_DIMENSIONS, (hexapod real measurments)
             "gaitParams": {
                 "tx": 0,
                 "tz": 0,
@@ -110,43 +82,107 @@ cases = [
                 "hipStance": 25,
                 "stepCount": 4,
                 "hipSwing": 25,
-                "liftSwing": 40,
-            },
-        },
-        "result": {"answer": True},
-        "description": "first sequence",
-    },
-]
+                "liftSwing": 40},
+        "gaitType": 'tripod', # ripple (can be customized)
+        'walkMode': 'walking', # rotating
+        """
 
+        self.x = 0
+        self.y = 0
+        self.face_direction = 0
+        self.servo_translator = LegTranslator(cmd)
+
+        self.params = params
+
+
+    def update_params(self, new_params: dict):
+        got_changes = True
+        for key, val in new_params.items():
+            if self.params[key] != val:
+                self.params[key] = val
+                got_changes = True
+        if got_changes:
+            self.update_sequence()
+
+    def update_sequence(self):
+        sequence, r_len = getWalkSequence(
+            dimensions=self.params["dimensions"],
+            params=self.params["gaitParams"],
+            gaitType=self.params['gaitType'],
+            walkMode=self.params['walkMode'])
+
+        self.sequence = sequence
+
+        alphas = self.sequence['rightMiddle']['alpha']
+        angle_diff = abs(max(alphas) - min(alphas))
+
+        l_projected = r_len.x*0.9
+
+        self.step_angle = int(angle_diff)
+        self.step_distance = int(l_projected*np.cos(np.radians(90-angle_diff/2))*2)*2  # double step
+
+        print("Unit angle, distance", self.step_angle, self.step_distance)
+
+    def forward(self, distance, rate):
+        self.update_params({"walkMode": 'walking'})
+
+        times = distance//self.step_distance
+        self.servo_translator.run_sequence(self.sequence, times, rate, rev=1)
+
+        self.x += self.step_distance*times * np.sin(np.radians(self.face_direction))
+        self.y += self.step_distance*times * np.cos(np.radians(self.face_direction))
+
+
+    def backward(self, distance, rate):
+        self.update_params({"walkMode": 'walking'})
+
+        times = distance//self.step_distance
+        self.servo_translator.run_sequence(self.sequence, times, rate, rev=-1)
+
+        self.x -= self.step_distance*times * np.sin(np.radians(self.face_direction))
+        self.y -= self.step_distance*times * np.cos(np.radians(self.face_direction))
+
+
+    def turn_left(self, degrees, rate):
+        self.update_params({"walkMode": 'rotating'})
+
+        times = degrees//self.step_angle
+        self.servo_translator.run_sequence(self.sequence, times, rate, rev=1)
+
+        result_angle_diff = self.step_angle * times
+        self.face_direction = (self.face_direction + result_angle_diff) % 360
+
+    def turn_right(self, degrees, rate):
+        self.update_params({"walkMode": 'rotating'})
+
+        times = degrees // self.step_angle
+        self.servo_translator.run_sequence(self.sequence, times, rate, rev=-1)
+
+        result_angle_diff = self.step_angle * times
+        self.face_direction = (self.face_direction + result_angle_diff) % 360
+
+
+"""Usage"""
 if __name__ == "__main__":
+
+    cmd = BusCmd()
+    cmd.startSerialProcess()
+    cmd.moveServos([i for i in range(1,19)],BASE_POS, 1000)
+
+    engine = LegMovement(cmd)
+    print("coords:", engine.x, engine.y, engine.face_direction)
+
+
     try:
-        # prepare hexapod
-        cmd.startSerialProcess()
-        get_initial_stance()
+        engine.forward(400,100)
+        print("coords:", engine.x, engine.y, engine.face_direction)
+        engine.backward(200, 100)
 
+        engine.turn_right(45, 100)
 
-        example = cases[0]
-        stepCount = example["params"]["gaitParams"]["stepCount"]
+        print("coords:", engine.x, engine.y, engine.face_direction)
+        engine.turn_left(90, 100)
+        print("coords:", engine.x, engine.y, engine.face_direction)
 
-        sequence = getWalkSequence(
-            dimensions=example["params"]["dimensions"],
-            params=example["params"]["gaitParams"],
-            gaitType="tripod", # "tripod"
-            walkMode='rotating' # rotating
-        )
-        # print(sequence['leftBack']["alpha"])
-
-        # leftFront: {alpha: [....], ..., leftRight:}
-        command_sequence = translateToServos(sequence)
-        for _ in range(4):
-            for seq in command_sequence:
-                run_pos(seq, 100)
-
-
-
-    except Exception as e:
-        print(e)
     finally:
-        time.sleep(0.5)
         cmd.powerOffServos()
-        time.sleep(0.5)
